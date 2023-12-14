b2c899a608f84e19356b08c8b359410a60819b9e
Eudneygabriel <eudneygabriel@epfundao.edu.pt>
2023-12-14 17:05:18 +0000

    first commit

--- scrip.js
+++ scrip.js
@@ -1,15 +1,5 @@
 // script.js
 
-function toggleText(elementId) {
-  var element = document.getElementById(elementId);
-
-  // Verifica se o elemento existe
-  if (element) {
-    // Alterna a visibilidade do texto usando a propriedade style.display
-    if (element.style.display === "none" || element.style.display === "") {
-      element.style.display = "block";
-    } else {
-      element.style.display = "none";
-    }
-  }
+function voltarParaPaginaInicial() {
+  window.location.href = "index.html"; // Redireciona para a página inicial
 }
