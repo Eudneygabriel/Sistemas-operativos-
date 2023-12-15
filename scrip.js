7be46e1f176ab36e400ac3543767ad35858899be
Eudneygabriel <eudneygabriel@epfundao.edu.pt>
2023-12-15 15:25:53 +0000

    first commit

--- scrip.js
+++ scrip.js
@@ -1,5 +1,17 @@
 // script.js
 
-function voltarParaPaginaInicial() {
-  window.location.href = "index.html"; // Redireciona para a página inicial
-}
+document.addEventListener("DOMContentLoaded", function () {
+  // Adiciona um ouvinte de evento ao botão
+  var toggleButton = document.getElementById("toggleButton");
+  toggleButton.addEventListener("click", function () {
+    // Obtém o elemento de texto pelo ID
+    var texto1 = document.getElementById("texto1");
+
+    // Alterna a visibilidade do texto usando a propriedade style.display
+    if (texto1.style.display === "none" || texto1.style.display === "") {
+      texto1.style.display = "block";
+    } else {
+      texto1.style.display = "none";
+    }
+  });
+});
